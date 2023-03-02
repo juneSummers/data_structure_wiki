a546ae2db82fe40ee36a3f029184ce8caf3369cb
juneSummers <junesummers686@gmail.com>
2023-03-02 17:20:03 +0800

    Array Initialization
    2d Array for data structure info declared and initialized.

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,20 @@ using System.Windows.Forms;
 
 namespace wikiPrototype
 {
+    
     public partial class DataStructureWiki : Form
     {
+     
         public DataStructureWiki()
         {
             InitializeComponent();
         }
+
+        static int rowSize = 12;
+
+        static int columnSize = 4;
+
+        string[,] dataStructureArray = new string[rowSize, columnSize];
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -30,6 +38,17 @@ namespace wikiPrototype
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void InitialiseArray()
+        {
+            for(int i = 0; i < rowSize; i++)
+            {
+                dataStructureArray[i, 0] = "~"; //Name
+                dataStructureArray[i, 1] = ""; //Category
+                dataStructureArray[i, 2] = ""; //Structure
+                dataStructureArray[i, 3] = ""; //Definition
+            }
         }
     }
 }
